30397f2e78dfc37bd4ab9767c5dc727be6dd8431
AstralFreak <pablonogueira@outlook.com>
2017-06-19 17:03:40 -0500

    FALTAN LOS TIMER, FALTA REPORTE, FALTA AJUSTE GRAFICO. TODO LO DEMÁS FUNCIONA

--- src/mx/unam/psicologia/MainWindow.java
+++ src/mx/unam/psicologia/MainWindow.java
@@ -1,8 +1,13 @@
 package mx.unam.psicologia;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by pablo on 18/06/17.
@@ -44,6 +49,52 @@ public class MainWindow {
     private JLabel labelConfirmaNombres;
     private JLabel labelConfirmaAplicador;
     private JLabel labelConfirmaDatos;
+    /**
+     * Elementos panelReady
+     */
+    private JPanel panelReady;
+
+    private JLabel labelReady;
+    /**
+     * Elementos panelPregunta1
+     */
+    private JPanel panelPregunta1;
+    private JLabel labelP1R42;
+    private JLabel labelP1R32;
+    /**
+     * Elementos panelPregunta2
+     */
+    private JPanel panelPregunta2;
+    private JLabel labelP2R32;
+    private JLabel labelP2R22;
+    /**
+     * Elementos panelPregunta3
+     */
+    private JPanel panelPregunta3;
+    private JLabel labelP3R1000;
+    private JLabel labelP3R0001;
+    /**
+     * Elementos panelWait
+     */
+    private JPanel panelWait;
+    private JButton buttonMostrarReporte;
+    private JPanel panelReporte;
+    private JPanel panelResultadosCuantitativos;
+    private JButton button1;
+    private JLabel labelReporteParticipante;
+    private JLabel labelReporteNumeroCaso;
+    private JLabel labelReporteNumeroEnsayo;
+    private JLabel labelReporteFecha;
+    private JLabel labelReporteAplicador;
+    private JLabel labelReporteRespuesta1;
+    private JLabel labelReporteRespuesta2;
+    private JLabel labelReporteRespuesta3;
+    private JLabel labelReporteCalificacion1;
+    private JLabel labelReporteCalificacion2;
+    private JLabel labelReporteCalificacion3;
+    private JLabel labelReporteTimer1;
+    private JLabel labelReporteTimer2;
+    private JLabel LabelReporteTimer3;
 
     /**
      *
@@ -55,6 +106,18 @@ public class MainWindow {
     String apellidos;
     String nombres;
     String aplicador;
+    int r1;
+    int r2;
+    int r3;
+    int verdad1;
+    int verdad2;
+    int verdad3;
+
+    /**
+     * Obtener la resolucion del dispositivo
+     */
+    static GraphicsDevice device = GraphicsEnvironment
+            .getLocalGraphicsEnvironment().getScreenDevices()[0];
 
     /**
      *
@@ -128,6 +191,150 @@ public class MainWindow {
             public void actionPerformed(ActionEvent e) {
                 panelConfirmacion.setVisible(false);
                 panelDatos.setVisible(true);
+            }
+        });
+        buttonContinuarAEnsayo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                panelConfirmacion.setVisible(false);
+                device.setFullScreenWindow(null);
+                panelReady.setVisible(true);
+
+                /**
+                 * Get Date
+                 */
+
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+                LocalDate localDate = LocalDate.now();
+
+                /**
+                 * Set Final cualitative values in report labels
+                 */
+
+                labelReporteParticipante.setText(apellidos + " " + nombres);
+                labelReporteNumeroCaso.setText(numeroCaso);
+                labelReporteNumeroEnsayo.setText(numeroEnsayo);
+                labelReporteAplicador.setText(aplicador);
+                labelReporteFecha.setText(String.valueOf(localDate));
+
+            }
+        });
+
+        /**
+         * Acciones panelReady
+         */
+
+        labelReady.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                panelReady.setVisible(false);
+                panelPregunta1.setVisible(true);
+                super.mouseClicked(e);
+            }
+        });
+
+        /**
+         * Acciones panelPregunta1
+         */
+        labelP1R42.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                r1 = 42;
+                verdad1 = 1;
+                labelReporteRespuesta1.setText(String.valueOf(r1));
+                labelReporteCalificacion1.setText("Correcta");
+                panelPregunta1.setVisible(false);
+                panelPregunta2.setVisible(true);
+                super.mousePressed(e);
+            }
+        });
+        labelP1R32.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                r1 = 32;
+                verdad1 = 0;
+                labelReporteRespuesta1.setText(String.valueOf(r1));
+                labelReporteCalificacion1.setText("Incorrecta");
+                panelPregunta1.setVisible(false);
+                panelPregunta2.setVisible(true);
+                super.mousePressed(e);
+            }
+        });
+
+        /**
+         * Acciones panelPregunta2
+         */
+        labelP2R32.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                r2 = 32;
+                verdad2 = 1;
+                labelReporteRespuesta2.setText(String.valueOf(r2));
+                labelReporteCalificacion2.setText("Correcta");
+                panelPregunta2.setVisible(false);
+                panelPregunta3.setVisible(true);
+                super.mousePressed(e);
+            }
+        });
+        labelP2R22.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                r2 = 22;
+                verdad2 = 0;
+                labelReporteRespuesta2.setText(String.valueOf(r2));
+                labelReporteCalificacion2.setText("Incorrecta");
+                panelPregunta2.setVisible(false);
+                panelPregunta3.setVisible(true);
+                super.mousePressed(e);
+            }
+        });
+
+        /**
+         * Acciones panelPregunta3
+         */
+
+        labelP3R1000.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                r3 = 1000;
+                verdad3 = 1;
+                labelReporteRespuesta3.setText(String.valueOf(r3));
+                labelReporteCalificacion3.setText("Correcta");
+                panelPregunta3.setVisible(false);
+                panelWait.setVisible(true);
+                super.mousePressed(e);
+            }
+        });
+        labelP3R0001.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                r3 = 0001;
+                verdad3 = 0;
+                labelReporteRespuesta3.setText(String.valueOf(r3));
+                labelReporteCalificacion3.setText("Incorrecta");
+                panelPregunta3.setVisible(false);
+                panelWait.setVisible(true);
+                super.mousePressed(e);
+            }
+        });
+        /**
+         * Acciones PanelWait
+         */
+        buttonMostrarReporte.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                panelWait.setVisible(false);
+                panelReporte.setVisible(true);
+            }
+        });
+
+        /**
+         * Acciones panelReporte
+         */
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
             }
         });
     }
